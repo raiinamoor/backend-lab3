6098f6843d6603c198569e056e9cd8ac5e54906e
Raii <raiinamoor@gmail.com>
2024-02-21 15:51:53 +0300

    Добавить класс Notification, использующий ILoggerService в качестве зависимости

---
+++ lab2/Notification.cs
@@ -0,0 +1,20 @@
+﻿namespace lab2
+{
+    public class Notification
+    {
+        // зависимость от логгера
+        ILoggerService _logger;
+
+        // конструктор, устанавливающий зависимость при создании экземпляра класса
+        public Notification(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+        public void Print(string message)
+        {
+            Console.WriteLine("Выполняется метод Print() сервиса Notification...");
+            _logger.Log(message);
+        }
+    }
+}
