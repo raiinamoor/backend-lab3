1da4c47fc31c7c7d74932d87f0bf14a82f920b5b
Raii <raiinamoor@gmail.com>
2024-02-21 15:50:28 +0300

    Добавить интерфейс ITimeService и класс TimeService для возвращения текущего времени

---
+++ lab2/TimeService.cs
@@ -0,0 +1,14 @@
+﻿namespace lab2
+{
+    // интерфейс, определяющий зависимость от сервиса, возвращающего текущее время
+    public interface ITimeService
+    {
+        string CurrentTime();
+    }
+
+    // реализация зависимости
+    public class TimeService : ITimeService
+    {
+        public string CurrentTime() => DateTime.Now.ToShortTimeString();
+    }
+}
