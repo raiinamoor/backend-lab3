5bcf3862a404be5ab12678325fe1a182aa73ec02
Raii <raiinamoor@gmail.com>
2024-02-21 15:51:09 +0300

    Добавить интерфейс ILoggerService и классы ConsoleLogger и ConsoleLoggerWithTime для логгирования данных

---
+++ lab2/Logger.cs
@@ -0,0 +1,30 @@
+﻿namespace lab2
+{
+    // интерфейс, определяющий зависимость от сервиса, логгирующего данные
+    public interface ILoggerService
+    {
+        void Log(string message);
+    }
+
+    // реализация зависимости с простым выводом сообщения в консоль
+    public class ConsoleLogger : ILoggerService
+    {
+        public void Log(string message) => Console.WriteLine(message);
+    }
+    
+    // реализация зависимости с выводом в консоль сообщения и текущего времени
+    public class ConsoleLoggerWithTime : ILoggerService
+    {
+        // зависимость
+        ITimeService _timeService;
+
+        // конструктор, устанавливающий зависимость при создании экземпляра класса
+        public ConsoleLoggerWithTime(ITimeService timeService)
+        {
+            _timeService = timeService;
+        }
+
+        public void Log(string message) => 
+            Console.WriteLine($"Message at {_timeService.CurrentTime()}: {message}");
+    }
+}
